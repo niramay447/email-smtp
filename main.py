8e6d9c2cfd4580c53a238975a58cf681b070aeef
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-09 12:51:49 +0530

    read csv using pandas

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from datetime import datetime
+import pandas
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+new_dict = {(data_row.month,data_row.day): data for (index, data_row) in data.iterrows()}
