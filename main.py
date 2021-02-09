70ebaa957def1cacf098a347b0f0f4094dbbfebb
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-09 12:57:04 +0530

    check for a birthday match

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 import pandas
-
+import random
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
 
-new_dict = {(data_row.month,data_row.day): data for (index, data_row) in data.iterrows()}
+birthdays_dict = {(data_row.month,data_row.day): data for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
