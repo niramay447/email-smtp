6c1d470cc5ab20feeca14fc27c34e3153045b7d7
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-09 13:09:54 +0530

    send the mail

--- main.py
+++ main.py
@@ -22,3 +22,8 @@ if today_tuple in birthdays_dict:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{contents}"
+        )
